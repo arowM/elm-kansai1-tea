5fee5ad333368d4bdb9c5d3e83d316f018bb5839
Kadzuya Okamoto <arow.okamoto@gmail.com>
2019-08-06 20:52:37 +0900

    Initial files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Elm関西 #1 サンプルプログラム
+
+[Elm関西 #1](https://connpass.com/event/141426/) 「Elmアーキテクチャについて」の題材
+
+## ビルド
+
+```bash
+$ npm i && npm run build
+```
+
+## 開発サーバー
+
+```bash
+$ npm i && npm start
+```
---
+++ src/Main.elm
@@ -0,0 +1,192 @@
+module Main exposing (Buttons(..), Goat(..), Model, Msg(..), init, main, update, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events as Events
+
+
+main : Program () model msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- Model
+
+
+type alias Model =
+    { goat : Goat
+    , mainMessage : String
+    , subMessage : String
+    , buttons : Buttons
+    }
+
+
+type Goat
+    = NormalGoat
+    | LoserGoat
+    | DrawGoat
+    | WinnerGoat
+    | WildGoat
+
+
+type Buttons
+    = NoButtons
+    | GuuChokiPaaButtons
+    | AgainButton
+
+
+init : Model
+init =
+    { goat = NormalGoat
+    , mainMessage = "じゃ〜んけ〜ん"
+    , subMessage = "すきな ぼたんを おしてね！"
+    , buttons = GuuChokiPaaButtons
+    }
+
+
+
+-- Message
+
+
+type Msg
+    = PressGuu
+    | PressChoki
+    | PressPaa
+    | ClickGoat
+    | PressAgain
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        PressGuu ->
+            -- * ヤギ: 悔しそうなやぎ
+            -- * メインメッセージ: 「かち！」
+            -- * サブメッセージ: 「ヤギさんにも ようしゃが ないね！」
+            -- * ボタンエリア
+            --     * 「もういっかい」
+            { goat = LoserGoat
+            , mainMessage = "かち！"
+            , subMessage = "ヤギさんにも ようしゃが ないね！"
+            , buttons = AgainButton
+            }
+
+        PressChoki ->
+            -- * ヤギ: 絶妙な表情のヤギ
+            -- * メインメッセージ: 「あいこ！」
+            -- * サブメッセージ: 「きが あうね！」
+            -- * ボタンエリア
+            --     * 「もういっかい」
+            { goat = DrawGoat
+            , mainMessage = "あいこ！"
+            , subMessage = "きが あうね！"
+            , buttons = AgainButton
+            }
+
+        PressPaa ->
+            -- * ヤギ: 嬉しいヤギ
+            -- * メインメッセージ: 「まけ！」
+            -- * サブメッセージ: 「ほもさぴえんすって よわい いきものだね！」
+            -- * ボタンエリア
+            --     * 「もういっかい」
+            { goat = WinnerGoat
+            , mainMessage = "まけ！"
+            , subMessage = "ほもさぴえんすって よわい いきものだね！"
+            , buttons = AgainButton
+            }
+
+        ClickGoat ->
+            -- * ヤギ: 荒ぶるやぎ
+            -- * メインメッセージ: 「じゃ〜んけ〜ん」
+            -- * サブメッセージ: 「やぎさんじゃなくて ぼたんをおしてね！」
+            -- * ボタンエリア:
+            --     * 「ぐー」
+            --     * 「ちょき」
+            --     * 「ぱー」
+            { goat = WinnerGoat
+            , mainMessage = "じゃ〜んけ〜ん"
+            , subMessage = "やぎさんじゃなくて ぼたんをおしてね！"
+            , buttons = GuuChokiPaaButtons
+            }
+
+        PressAgain ->
+            -- 初期状態と一緒だからズルしちゃおう！
+            init
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        [ img
+            [ src <|
+                case model.goat of
+                    NormalGoat ->
+                        "./img/goat/normal.jpg"
+
+                    LoserGoat ->
+                        "./img/goat/loser.jpg"
+
+                    DrawGoat ->
+                        "./img/goat/draw.jpg"
+
+                    WinnerGoat ->
+                        "./img/goat/winner.jpg"
+
+                    WildGoat ->
+                        "./img/goat/wild.jpg"
+            , Events.onClick ClickGoat
+            ]
+            []
+        , h1
+            []
+            [ text model.mainMessage
+            ]
+        , h2
+            []
+            [ text model.subMessage
+            ]
+        , case model.buttons of
+            NoButtons ->
+                text ""
+
+            GuuChokiPaaButtons ->
+                div
+                    []
+                    [ button
+                        [ type_ "button"
+                        , Events.onClick PressGuu
+                        ]
+                        [ text "ぐー"
+                        ]
+                    , button
+                        [ type_ "button"
+                        , Events.onClick PressChoki
+                        ]
+                        [ text "ちょき"
+                        ]
+                    , button
+                        [ type_ "button"
+                        , Events.onClick PressPaa
+                        ]
+                        [ text "ぱー"
+                        ]
+                    ]
+
+            AgainButton ->
+                div
+                    []
+                    [ button
+                        [ type_ "button"
+                        , Events.onClick PressAgain
+                        ]
+                        [ text "もういっかい"
+                        ]
+                    ]
+        ]
